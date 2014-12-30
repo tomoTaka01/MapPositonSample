1d9235f0e2782cecd282fbb763dfbd214c6dbf2a
tomo taka <tomo_taka01@hotmail.com>
2014-12-30 22:59:36 +0900

    TODO save json file

--- src/mappositionsample/MapPositionSample.java
+++ src/mappositionsample/MapPositionSample.java
@@ -2,16 +2,18 @@ package mappositionsample;
 
 import java.net.URL;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
-
+import netscape.javascript.JSObject;
+import org.json.simple.JSONObject;
 /**
  *
  * @author tomo
@@ -24,9 +26,20 @@ public class MapPositionSample extends Application {
         WebEngine engine = webView.getEngine();
         URL url = getClass().getResource("sample.html");
         engine.load(url.toExternalForm());
+        JSObject win = (JSObject) engine.executeScript("window");
+        win.setMember("app", new JavaApp());
+        
         VBox root = new VBox(10);
-        root.getChildren().addAll(webView);
-        Scene scene = new Scene(root, 500, 500);
+        HBox hbox1 = new HBox(10);
+        hbox1.setPadding(new Insets(5));
+        hbox1.setAlignment(Pos.CENTER_RIGHT);
+        Button saveBtn = new Button("exit");
+        saveBtn.setOnAction(e -> {
+            Platform.exit();
+        });
+        hbox1.getChildren().addAll(saveBtn);
+        root.getChildren().addAll(webView, hbox1);
+        Scene scene = new Scene(root, 800, 600);
         
         stage.setTitle("Map Position Sample");
         stage.setScene(scene);
@@ -39,5 +52,12 @@ public class MapPositionSample extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+    
+public class JavaApp{
+    public void getInfoList(String infos){
+        System.out.println(infos);
+        // TODO save file
+    }
+}
     
 }
--- src/mappositionsample/sample.js
+++ src/mappositionsample/sample.js
@@ -1,7 +1,31 @@
+/*
+ * sample.js
+ */
 var map;
+var infos = [
+    {place: 'KIX', lat: 34.4342, lng: 135.2328},
+    {place: 'SFO', lat: 37.6189, lng: -122.3750}
+];
+
+/*
+ * initialize
+ */
 $(function(){
+   // init map
+   initMap();
+   // regist [save to json] button event
+   $('#saveBtn').on('click', saveJson);
+   // regist map event
+   google.maps.event.addListener(map, "click", addPoints);
+});
+
+/*
+ * initialize google map
+ */ 
+function initMap(){
     var mapCanvas = document.getElementById("map_canvas");
-    var myLatlng = new google.maps.LatLng(34.733758, 135.496849);
+    var kixLatLng = infos[0];
+    var myLatlng = new google.maps.LatLng(kixLatLng.lat, kixLatLng.lng);
     var mapOptions = {
         zoom: 12,
         center: myLatlng
@@ -21,80 +45,158 @@ $(function(){
     });
     //  set zoom
     map.fitBounds(bounds);
-    // Polyine
+    // draw KIX-SFO Polyine
     var polylineOpt = {
         map: map,
         path:paths,
         editable:true
     };
-    var lineObj = new google.maps.Polyline(polylineOpt);
-    google.maps.event.addListener(map, "click", addPoints);
-});
+    new google.maps.Polyline(polylineOpt);    
+}
 
-// add point infomation
+/*
+ * add point to map
+ */
 function addPoints(event){
     var myLatlng = event.latLng;
-    pointsInfo.addPoint(myLatlng);
-    // add tr
+    var infoOptions = {
+        latlng:myLatlng
+    };
+    pointsInfo.addPoint(infoOptions);
+    // add latest point to table 
+    addTr(pointsInfo.getPoint());
+}
+
+/*
+ * add tr element to table
+ */
+function addTr(pointObj){    
     var tbody = $('#points');
     var tr = $('<tr>');
     tr.appendTo(tbody);
-    $('<td>').text(pointsInfo.pointNo).addClass("latlngNo").appendTo(tr);
-    var infoObj = pointsInfo.infoList[pointsInfo.pointNo];
-    $('<td>').text(infoObj.lat).addClass("latlng").appendTo(tr);
-    $('<td>').text(infoObj.lng).addClass("latlng").appendTo(tr);
+    $('<td>').text(pointObj.pointNo+1).addClass("latlngNo").appendTo(tr);
+    $('<td>').text(pointObj.lat).addClass("latlng").appendTo(tr);
+    $('<td>').text(pointObj.lng).addClass("latlng").appendTo(tr);
     // description
     var tdDesc = $('<td>').addClass("description");
-    $('<input>').addClass("inputDesc").appendTo(tdDesc);
+    $('<input>').addClass("inputDesc").val(pointObj.desc)
+            .attr('selno', pointObj.pointNo)
+            .change(function(event){
+                var inDesc = $(this);
+                // set desc
+                var selNo = inDesc.attr('selno');
+                pointsInfo.setDesc(selNo, inDesc.val());
+            })
+            .appendTo(tdDesc);
     tdDesc.appendTo(tr);
     // linecolor
     var tdColor = $('<td>').addClass("linecolor");
-    $('<input>').attr('type', 'color').addClass("inputColor").appendTo(tdColor);
+    $('<input>').attr('type', 'color').val(pointObj.color).addClass("inputColor")
+            .attr('selno', pointObj.pointNo)
+            .change(function(event){
+                var inColor = $(this);
+                // set color
+                var selNo = inColor.attr('selno');
+                pointsInfo.setColor(selNo, inColor.val());
+            })
+            .appendTo(tdColor);
     tdColor.appendTo(tr);
-    // Button
+    // delete Button
     var tdBtn = $('<td>').addClass("btn");
-    $('<input>').attr('type', 'button').addClass("inputBtn").attr('value', 'delete').attr('name', 'deleteBtn')
-            .attr('deleteno', pointsInfo.pointNo)
+    $('<input>').attr('type', 'button').addClass("inputBtn").attr('value', 'delete')
+            .attr('deleteno', pointObj.pointNo)
             .click(function(event){
                 var delBtnObj = $(this);
                 // remove tr
                 var parentTr = delBtnObj.parent().parent();
                 parentTr.remove();
-                // reset no TODO
+                // delete pointsInfo
                 var delNo = delBtnObj.attr('deleteno');
                 pointsInfo.deletePoint(delNo);
+                pointsInfo.deleteMarker(delNo);
             })
             .appendTo(tdBtn);
     tdBtn.appendTo(tr);
     // create marker
-    var marker = new google.maps.Marker({
-        position: myLatlng,
-        map: map,
-        title:String(pointsInfo.pointNo)
-    });
-
+    pointsInfo.addMarker(pointObj);
 }
-
+/*
+ * point information 
+ */
 var pointsInfo = {
     pointNo:-1,
+    // pointNo, lat, lng, desc, color, latlng, isDeleted
     infoList:[],
-    addPoint:function(latlng){
+    markers:[],
+    getPoint:function(selNo){
+        if (selNo){
+            return this.infoList[selNo];
+        } else {
+            return this.infoList[this.pointNo];
+        }
+    },
+    addPoint:function(info){
         this.pointNo++;
         var infoObj = {
-            lat:latlng.lat(),
-            lng:latlng.lng()
+            isDeleted:false
         };
+        infoObj.pointNo = this.pointNo;
+        if (info.latlng){
+            infoObj.lat = info.latlng.lat();
+            infoObj.lng = info.latlng.lng();
+            infoObj.latlng = info.latlng;
+        }
+        if (info.desc){
+            infoObj.desc = info.desc;
+        } else {
+            infoObj.desc = '';
+        }
+        if (info.lat){
+            infoObj.lat = info.lat;
+        }
+        if (info.lng){
+            infoObj.lng = info.lng;
+        }
+        if (info.color){
+            infoObj.color = info.color;
+        } else {
+            infoObj.color = '#000000';
+        }
+        // this infoObj has pointNo, lat, lng, desc, color, latlng, isDeleted
         this.infoList.push(infoObj);
     },
+    addMarker:function(pointObj){
+        var marker = new google.maps.Marker({
+            position: pointObj.latlng,
+            map: map,
+            title:String(pointObj.pointNo+1)
+        });        
+        this.markers.push(marker);
+    },
+    setColor:function(selNo, selColor){
+        var info = this.infoList[selNo];
+        info.color = selColor;
+    },
+    setDesc:function(selNo, inDesc){
+        var info = this.infoList[selNo];
+        info.desc = inDesc;
+    },
     deletePoint:function(delNo){
-        this.infoList.splice(delNo, 1); // remove line from array
-        this.pointNo--;
-        alert(delNo + "," + this.pointNo);
+        //this.infoList.splice(delNo, 1); // remove line from array
+        this.infoList[delNo].isDeleted = true;
+    },
+    deleteMarker:function(delNo){
+        this.markers[delNo].setMap(null);
+    },
+    getInfoList:function(){
+        return this.infoList;
     }
-    
 };
-
-var infos = [
-    {place: 'KIX', lat: 34.4342, lng: 135.2328},
-    {place: 'SFO', lat: 37.6189, lng: -122.3750}
-];
+/*
+ * [save to json] button event
+ */        
+function saveJson(){
+    // call java method
+    var infos = JSON.stringify(pointsInfo.infoList);
+    app.getInfoList(infos);
+}
